426c813a4971eb47acfbe88c44929a2b0d2b02d3
OmidAfzali02 <dev.omid02@gmail.com>
2024-07-03 00:00:15 +0330

    meal model added to admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Ingredient
+from .models import User, Ingredient, Meal
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Ingredient)
+admin.site.register(Meal)
