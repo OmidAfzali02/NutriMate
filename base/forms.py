b3c4af728444714c57ae1eaad3dd1da117c40bcd
OmidAfzali02 <dev.omid02@gmail.com>
2024-07-02 23:15:33 +0330

    user creation form added

---
+++ base/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'phone', 'password1', 'password2', )
