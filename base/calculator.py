17ba56f366e03d8a47348f8eafb57982dd72f49b
OmidAfzali02 <dev.omid02@gmail.com>
2024-07-06 22:45:00 +0330

    added some comments

--- base/calculator.py
+++ base/calculator.py
@@ -1,4 +1,4 @@
-def nutritional_calc(data, ourData):
+def nutritional_calc(data, ourData): # to calculate the total nutritions in all of the ingredients
     total_calorie = 0
     total_protein = 0
     total_carbohydrates = 0
@@ -6,38 +6,38 @@ def nutritional_calc(data, ourData):
     total_sugar = 0
     total_creatine = 0
     total_glutamine = 0
-    for key,value in data.items():
+    for key,value in data.items(): # to get each of the ingredients in user input
         value = float(value)
-        base_data = ourData.get(key)
+        base_data = ourData.get(key) # ourdata is the information of each ingrediant and their nutritional values
         calorie = base_data.get('calorie')
         calorie_gain = value * calorie /100
         total_calorie += calorie_gain
 
         protein = base_data.get('protein')
-        protein_gain = value*protein/100
+        protein_gain = value * protein/100
         total_protein += protein_gain
 
         carbohydrates = base_data.get('carbohydrates')
-        carbohydrates_gain = value*carbohydrates/100
+        carbohydrates_gain = value * carbohydrates/100
         total_carbohydrates += carbohydrates_gain
 
         fat = base_data.get('fat')
-        fat_gain = value*fat/100
+        fat_gain = value * fat/100
         total_fat += fat_gain
 
         sugar = base_data.get('sugar')
-        sugar_gain = value*sugar/100
+        sugar_gain = value * sugar/100
         total_sugar += sugar_gain
 
         creatine = base_data.get('creatine')
-        creatine_gain = value*creatine/100
+        creatine_gain = value * creatine/100
         total_creatine += creatine_gain
 
         glutamine = base_data.get('glutamine')
-        glutamine_gain = value*glutamine/100
+        glutamine_gain = value * glutamine/100
         total_glutamine += glutamine_gain
 
-    result = {
+    result = { # the final output
         'total_calorie':round(total_calorie, 2),
         'total_protein': round(total_protein, 2),
         'total_carbohydrates':round(total_carbohydrates, 2),
@@ -46,5 +46,4 @@ def nutritional_calc(data, ourData):
         'total_creatine':round(total_creatine, 2),
         'total_glutamine':round(total_glutamine, 2),
     }
-
     return result
